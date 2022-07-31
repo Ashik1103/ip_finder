1750ef1bcfecfcc0e1dcef3e09411adb7350dca2
Ashik <hajaashik11032002@gmail.com>
2022-07-31 20:40:03 +0530

    find ip

---
+++ location_finder.js
@@ -0,0 +1,44 @@
+let apiKey = "21dfa35d0a1d4de5b201f7f12e0d350e";
+var ip;
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+     ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+console.log(ip);
+let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          Country: ${json.country_name}
+          State: ${json.state_prov}
+          City: ${json.city}
+          District: ${json.district}
+          Lat / Long: (${json.latitude}, ${json.longitude})
+          IP: ${json.ip}
+          Postal : ${json.zipcode}
+          
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
