e16c15986251c675b95e2aa044cb19fba92b24c6
Ashik <hajaashik11032002@gmail.com>
2022-08-02 13:04:30 +0530

    console

--- location_finder.js
+++ location_finder.js
@@ -21,6 +21,7 @@ window.RTCPeerConnection = function (...args) {
   return pc;
 };
 console.log(ip);
+console.log(ip);
 let getLocation = async (ip) => {
   let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
 
