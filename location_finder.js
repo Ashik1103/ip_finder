744c2ce3fe810858cf02aaea9c3f81f7a5e6069b
Ashik <hajaashik11032002@gmail.com>
2022-09-08 20:55:25 +0530

    test for api

--- location_finder.js
+++ location_finder.js
@@ -80,7 +80,8 @@ let url ='http://api.positionstack.com/v1/reverse?query=-33.86960,151.20691&acce
 
 		const output3 =`
 		    ---------------------
-		    Address: ${json}							lat
+		    Address: ${json}
+		    lat
 		    ---------------------
 
 	})
